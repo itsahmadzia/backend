f796e0900577a0ec3b4ecc4aee41c23126ea6ae7
ahmad zia <mahmad2004a8@gmail.com>
2024-03-09 23:14:40 +0500

    JWT methods added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,7 +58,37 @@ next();
  
 
 })
-userSchema.methods=async function(pass){
+userSchema.methods.isPasswordCorrect =async function(pass){
    return await (bcrypt.compare(pass,this.password))
 }
+
+userSchema.methods.generateRefreshToken= function(){
+      jwt.sign(
+            {
+                  _id: this._id,
+              
+            }
+            ,
+            process.env.REFRESH_TOKEN_SECRET ,
+            {
+                  expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            }
+         )
+}
+
+userSchema.methods.generateAccessToken= function(){
+   return jwt.sign(
+      {
+            _id: this._id,
+            email:this.email,
+            username: this.username,
+            fullName: this.fullName
+      }
+      ,
+      process.env.ACCESS_TOKEN_SECRET ,
+      {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+      }
+   )
+   }
 export const User = mongoose.model("User", userSchema);
